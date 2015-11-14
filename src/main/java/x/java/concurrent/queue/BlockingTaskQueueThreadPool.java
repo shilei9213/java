fdde06a3f13b0fee37a527d53f23edf0d06eac83
shilei9213 <shilei9213@126.com>
2015-11-14 17:20:47 +0800

    带阻塞队列的线程池

---
+++ src/main/java/x/java/concurrent/queue/BlockingTaskQueueThreadPool.java
@@ -0,0 +1,58 @@
+package x.java.concurrent.queue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 阻塞任务队列线程池
+ * 
+ * @author shilei
+ *
+ */
+public class BlockingTaskQueueThreadPool {
+	private BlockingQueue<Runnable> taskQueue;
+	private ThreadPoolExecutor threadPool;
+
+	public BlockingTaskQueueThreadPool(int threadPoolSize, int taskQueueSize) {
+		taskQueue = new LinkedBlockingQueue<Runnable>(taskQueueSize);
+		threadPool = new ThreadPoolExecutor(threadPoolSize, threadPoolSize, 0L, TimeUnit.MILLISECONDS, taskQueue, new RejectedExecutionHandler() {
+
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+				if (!executor.isShutdown()) {
+					try {
+						executor.getQueue().put(r);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+
+		});
+	}
+
+	/**
+	 * 提交任务，会阻塞
+	 * 
+	 * @param task
+	 */
+	public void submit(Runnable task) {
+		threadPool.execute(task);
+	}
+
+	/**
+	 * 等待结束
+	 */
+	public void waitTermianl() {
+		threadPool.shutdown();
+
+		try {
+			while (!threadPool.isTerminated()) {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
+}
---
+++ src/test/java/x/java/concurrent/queue/BlockingTaskQueueThreadPoolTest.java
@@ -0,0 +1,44 @@
+package x.java.concurrent.queue;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class BlockingTaskQueueThreadPoolTest extends TestCase {
+
+	@Test
+	public void test() {
+		BlockingTaskQueueThreadPool taskQueue = new BlockingTaskQueueThreadPool(1, 5);
+		for (int i = 0; i < 10; i++) {
+			taskQueue.submit(new Work(i));
+			System.out.println(Thread.currentThread().getName() + " add task " + i);
+		}
+
+		System.out.println(Thread.currentThread().getName() + " add finish！ ");
+
+		taskQueue.waitTermianl();
+
+		System.out.println("all finish！ ");
+	}
+
+	public static class Work implements Runnable {
+		private int no;
+
+		public Work(int no) {
+			this.no = no;
+		}
+
+		@Override
+		public void run() {
+
+			System.out.println(Thread.currentThread().getName() + " start " + no);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " finish " + no);
+
+		}
+	}
+}
