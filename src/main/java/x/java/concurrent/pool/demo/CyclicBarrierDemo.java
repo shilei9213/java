1981d5b7ba66818100c15e49a6bff33047fb5098
shilei0907 <shilei0907@autohome.com>
2020-08-11 17:55:43 +0800

    线程

--- src/main/java/x/java/concurrent/pool/demo/CyclicBarrierDemo.java
+++ src/main/java/x/java/concurrent/pool/demo/CyclicBarrierDemo.java
@@ -6,7 +6,9 @@ import java.util.concurrent.CyclicBarrier;
 /**
  * CyclicBarrier: 可循环使用（Cyclic）的屏障（Barrier), 让一组线程到达一个屏障（也可以叫同步点）时被阻塞，直到最后一个线程到达屏障时，屏障才会开门，所有被屏障拦截的线程才会继续干活。
  * <p>
- * CountDownLatch的计数器只能使用一次。而CyclicBarrier的计数器可以使用reset() 方法重置。所以CyclicBarrier能处理更为复杂的业务场景，比如如果计算发生错误，可以重置计数器，并让线程们重新执行一次
+ * CountDownLatch的计数器只能使用一次。
+ * 而CyclicBarrier的计数器可以使用reset() 方法重置。
+ * 所以CyclicBarrier能处理更为复杂的业务场景，比如如果计算发生错误，可以重置计数器，并让线程们重新执行一次
  *
  * @author shilei0907
  * @version 1.0, 2018/7/8
